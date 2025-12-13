d2a9708217dc8a8dbdba79fe63f225e057662505
Luke Waldron <lukewaldron87@gmail.com>
2025-12-13 10:02:26 +0000

    starvation example

---
+++ 15-thread-problems/src/main/java/org/example/StarvingThread.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class StarvingThread {
+
+    private static final Lock lock = new ReentrantLock(true);
+
+    public static void main(String[] args) {
+
+        Callable<Boolean> thread = () -> {
+            String threadName = Thread.currentThread().getName();
+            int threadNumber = Integer.parseInt(
+                    threadName.replaceAll(".*-[1-9]*-.*-", ""));
+            boolean keepGoing = true;
+            while (keepGoing) {
+                lock.lock();
+                try {
+                    System.out.printf("%d acquired the lock.%n", threadNumber);
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    System.out.printf("Shutting down %d%n", threadNumber);
+                    Thread.currentThread().interrupt();
+                    return false;
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+            return true;
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        try {
+            List<Future<Boolean>> futures = executor.invokeAll(
+                    List.of(thread, thread, thread),
+                    10,
+                    TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        executor.shutdownNow();
+    }
+}
